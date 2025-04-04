9f5cb25d3a7ca64613eb2c25fd74b2d6a91b8e6b
j0rgef <jorgemiguelzito@gmail.com>
2025-04-04 23:58:07 +0100

    Removing unnecessary line from ListComprehension

--- ListComprehension.py
+++ ListComprehension.py
@@ -7,8 +7,6 @@ if __name__ == '__main__':
     n = int(input())
     
     coordinates = [x, y ,z]
-    cp = []
-    
     cp = [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1)]
     
     cpn = [d for d in cp if sum(d) != n]
